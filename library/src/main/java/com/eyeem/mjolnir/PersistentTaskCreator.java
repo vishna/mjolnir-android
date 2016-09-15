1589cb9ce3c7530ecb3ae13b721dccafb5703520
Lukasz Wisniewski <luke.cherrish@gmail.com>
2016-09-15 14:43:12 +0200

    remove instagram, remove persistent task service

---
+++ library/src/main/java/com/eyeem/mjolnir/PersistentTaskCreator.java
@@ -0,0 +1,15 @@
+package com.eyeem.mjolnir;
+
+import com.evernote.android.job.Job;
+import com.evernote.android.job.JobCreator;
+
+/**
+ * Created by vishna on 15/09/16.
+ */
+
+public class PersistentTaskCreator implements JobCreator {
+
+   @Override public Job create(String tag) {
+      return null;
+   }
+}
